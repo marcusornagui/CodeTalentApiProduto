aa9eae32b3d0365479e370a5c564df5b9fddae6a
marcusornagui <marcusornagui@gmail.com>
2024-07-09 08:29:18 -0300

    Criação de estrutura de pastar e configurações iniciais.

---
+++ .docker/node/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Container started"
+
+npm install --legacy-peer-deps
+
+npm run start:debug
---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { NestFactory } from '@nestjs/core';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { ResponseExceptionsFilter } from './shared/filters/response-exception.filter';
+import { ResponseTransformInterceptor } from './shared/interceptors/response-transform.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(new ResponseTransformInterceptor());
+  app.useGlobalFilters(new ResponseExceptionsFilter());
+
+  app.enableCors();
+
+  setupOpenAPI(app);
+
+  await app.listen(3001);
+}
+bootstrap();
+
+function setupOpenAPI(app: INestApplication): void {
+  const config = new DocumentBuilder().setTitle('CodeTalentApiProduto').build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document, { useGlobalPrefix: true });
+
+  Logger.log('OpenAPI is running on http://localhost:3001/api/v1/docs');
+}
